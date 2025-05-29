7c221a2c899ca9673726f5ff726111c59602c85e
Tchayman <96773825+tchayman@users.noreply.github.com>
2025-05-29 02:30:13 +0200

    Update decoder.py

--- src/thor_fp/decoder.py
+++ src/thor_fp/decoder.py
@@ -6,11 +6,25 @@ Author: Alexandre ICHAÏ (www.thor.love)
 import numpy as np
 
 class DeltaPhiDecoder:
+    """
+    DeltaPhiDecoder applies a decoding operation based on ΔΦ.
+    """
+
     def _init_(self, delta_phi=0.1):
         self.delta_phi = delta_phi
 
     def decode(self, y):
         """
-        Example decoding function (to be customized).
+        Decode input y using ΔΦ.
+
+        Parameters
+        ----------
+        y : float or np.ndarray
+            Input value(s).
+
+        Returns
+        -------
+        float or np.ndarray
+            Decoded value(s).
         """
         return y / self.delta_phi
