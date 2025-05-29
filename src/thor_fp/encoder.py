8aa9519961375f09c60f2ce575a2f5d907fd3504
Tchayman <96773825+tchayman@users.noreply.github.com>
2025-05-29 02:29:39 +0200

    Update encoder.py

--- src/thor_fp/encoder.py
+++ src/thor_fp/encoder.py
@@ -6,12 +6,26 @@ Author: Alexandre ICHAÏ (www.thor.love)
 import numpy as np
 
 class FractalEncoder:
+    """
+    FractalEncoder applies fractal encoding using π and φ.
+    """
+
     def _init_(self, pi=np.pi, phi=(1 + 5 ** 0.5) / 2):
         self.pi = pi
         self.phi = phi
 
     def encode(self, x):
         """
-        Example encoding function (to be customized).
+        Encode input x using fractal/harmonic structure.
+
+        Parameters
+        ----------
+        x : float or np.ndarray
+            Input value(s).
+
+        Returns
+        -------
+        float or np.ndarray
+            Encoded value(s).
         """
         return np.sin(self.pi * x) + np.cos(self.phi * x)
