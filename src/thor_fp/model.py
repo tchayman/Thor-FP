c53124473bc028d20c0d9ed016e84f9e8a3d0b31
Tchayman <96773825+tchayman@users.noreply.github.com>
2025-05-29 02:28:52 +0200

    Update model.py

--- src/thor_fp/model.py
+++ src/thor_fp/model.py
@@ -7,11 +7,29 @@ Author: Alexandre ICHAÏ (www.thor.love)
 import numpy as np
 
 class THORModel:
+    """
+    THORModel implements the scalar topological law θ₀ = 42π.
+
+    Methods
+    -------
+    scalar_invariant(theta):
+        Compute the scalar invariant for a given angle theta.
+    """
     def _init_(self, theta0=42 * np.pi):
         self.theta0 = theta0
 
     def scalar_invariant(self, theta):
         """
         Compute the scalar invariant for an input angle theta.
+
+        Parameters
+        ----------
+        theta : float or np.ndarray
+            Angle(s) in radians.
+
+        Returns
+        -------
+        float or np.ndarray
+            Value of the scalar invariant at theta.
         """
         return self.theta0 * np.cos(theta)
